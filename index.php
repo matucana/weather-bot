81e1c6650807b631d109c07da459c026654be6a3
Dialog Box Studio <79014508+matucana@users.noreply.github.com>
2021-03-18 10:11:04 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,14 +3,12 @@ require_once __DIR__.'/vendor/autoload.php';
 
 use Dotenv\Dotenv;
 use Matucana\WeatherBot\App;
+use Matucana\WeatherBot\Handlers\VkHandler;
 
 Dotenv::createImmutable(__DIR__, '/config/.env')->load();
 
 
 $app = new App(['lat' => $_ENV['LAT'], 'lon' => $_ENV['LONG']]);
-$message =  $app->getMessageCurrentWeather();
-$vk = new \VK\Client\VKApiClient();
-$vk->messages()->send($_ENV['VK_API_KEY'],['peer_id' => $_ENV['VK_PEER_ID'], 'random_id' => mt_rand(0,99999999999999), 'message' => $message,'lat' => $_ENV['LAT'], 'long' => $_ENV['LONG'] ]);
-
-
-
+$vkHandler = new VkHandler($_ENV['VK_API_KEY'], $_ENV['VK_PEER_ID']);
+$app->addHandler($vkHandler);
+$app->run();
