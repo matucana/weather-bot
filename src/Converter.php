292df55dbad25aae76aea04d3a75445e5897a23b
Matucana <79014508+matucana@users.noreply.github.com>
2021-03-31 23:04:00 +0300

    PSR - 12
    code formatting by PSR - 12

--- src/Converter.php
+++ src/Converter.php
@@ -9,20 +9,40 @@ use DateTimeZone;
 
 class Converter
 {
-    public function dateTimeFormat(DateTime $DateTime, string $timeZone = 'Europe/Moscow', string $format = 'd.m.Y H:i:s'): string
-    {
+    public function dateTimeFormat(
+        DateTime $DateTime,
+        string $timeZone = 'Europe/Moscow',
+        string $format = 'd.m.Y H:i:s'
+    ): string {
         return $DateTime->setTimezone(new DateTimeZone($timeZone))->format($format);
     }
 
     public function pressureConversion(int $pressure): int
     {
-        return $pressure / 1.333;
+        return (int)$pressure / 1.333;
     }
 
     public function windConversion(int $degree): string
     {
         $direction = $degree / 22.5 + .5;
-        $cardinal_array = ["Северный", "Северно-Северо-Восточный", "Северо-Восточный", "Восточно-Северо-Восточный", "Восточный", "Восточно-Юго-Восточный", "Юго-Восточный", "SSE", "Южный", "Южно-Юго-Восточный", "Юго-Западный", "Западо-Юго-Западный", "Западный", "Западно-Северно-Западный", "Северо-Западный", "Северно-Северо-Западный"];
-        return $cardinal_array[ ( $direction % 16 )];
+        $cardinal_array = [
+            "Северный",
+            "Северно-Северо-Восточный",
+            "Северо-Восточный",
+            "Восточно-Северо-Восточный",
+            "Восточный",
+            "Восточно-Юго-Восточный",
+            "Юго-Восточный",
+            "SSE",
+            "Южный",
+            "Южно-Юго-Восточный",
+            "Юго-Западный",
+            "Западо-Юго-Западный",
+            "Западный",
+            "Западно-Северно-Западный",
+            "Северо-Западный",
+            "Северно-Северо-Западный"
+        ];
+        return $cardinal_array[($direction % 16)];
     }
 }
