ceac98dbfece0fc0528a9b7b0ea5b785a12a5435
Matucana <79014508+matucana@users.noreply.github.com>
2021-03-31 23:04:19 +0300

    Create ConverterTest.php

---
+++ tests/ConverterTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Matucana\WeatherBot\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class ConverterTest extends TestCase
+{
+    public function testDateTimeFormat()
+    {
+    }
+
+    public function testPressureConversion()
+    {
+    }
+
+    public function testWindConversion()
+    {
+    }
+
+}
