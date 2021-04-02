caa845aa728d42d33daef591982073319db5c68b
Matucana <79014508+matucana@users.noreply.github.com>
2021-04-02 23:44:22 +0300

    Create Settings.php

---
+++ src/Settings/Settings.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace Matucana\WeatherBot\Settings;
+
+
+class Settings implements SettingsInterface
+{
+    private array $settings;
+
+    public function __construct(array $settings)
+    {
+        $this->settings = $settings;
+    }
+    public function get(string $key)
+    {
+        return (empty($key))? $this->settings : $this->settings[$key];
+    }
+
+}
