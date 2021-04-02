3602e331089a49e9d88b3bc39af4653641b442ab
Matucana <79014508+matucana@users.noreply.github.com>
2021-04-02 23:45:05 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,14 +2,27 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-use Dotenv\Dotenv;
 use Matucana\WeatherBot\App;
 use Matucana\WeatherBot\Handlers\VkHandler;
+use Symfony\Component\Dotenv\Dotenv;
+use DI\ContainerBuilder;
 
-Dotenv::createImmutable(__DIR__, '/config/.env')->load();
+$dotenv = new Dotenv();
+$dotenv->load(__DIR__ . '/.env');
+
+$containerBuilder = new ContainerBuilder();
+
+$settings = require __DIR__ . '/app/settings.php';
+$settings($containerBuilder);
+
+$dependencies = require __DIR__ . '/app/dependencies.php';
+$dependencies($containerBuilder);
+
+$container = $containerBuilder->build();
 
 
-$app = new App(['lat' => $_ENV['LAT'], 'lon' => $_ENV['LONG']]);
-$vkHandler = new VkHandler($_ENV['VK_API_KEY'], $_ENV['VK_PEER_ID']);
-$app->addHandler($vkHandler);
+$app = $container->get(App::class);
+$app->addHandler($container->get(VkHandler::class));
 $app->run();
+
+
