4f304693734a74bc8cac38cc274c8c61d06044c7
Matucana <79014508+matucana@users.noreply.github.com>
2021-04-03 12:38:37 +0300

    Update ConverterTest.php

--- tests/ConverterTest.php
+++ tests/ConverterTest.php
@@ -2,21 +2,33 @@
 
 namespace Matucana\WeatherBot\Test;
 
+use DateTime;
+use Matucana\WeatherBot\Converter;
 use PHPUnit\Framework\TestCase;
 
 class ConverterTest extends TestCase
 {
     public function testDateTimeFormat()
     {
+        $converter = new Converter();
+        $dateTime = new DateTime('03.04.2021', new \DateTimeZone('Europe/Moscow'));
+        $dateTimeString = $converter->dateTimeFormat($dateTime, 'Europe/Moscow', 'd.m.Y');
+        $this->assertEquals('03.04.2021', $dateTimeString);
     }
 
     public function testPressureConversion()
     {
+        $converter = new Converter();
+        $pressure = $converter->pressureConversion(133);
+        $this->assertEquals(99, $pressure);
 
     }
 
     public function testWindConversion()
     {
+        $converter = new Converter();
+        $wind = $converter->windConversion(1);
+        $this->assertEquals('Северный', $wind);
     }
 
 }
